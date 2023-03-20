78cf76d7297c6b17baac3a11d60b097ac53ebe60
Bassey Duke <bassey.bd@gmail.com>
2023-03-20 15:36:19 -0400

    protfolio website

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    title: "React Task Tracker",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://github.com/Basseybd/react-task-tracker",
+  },
+  {
+    title: "React Task Tracker",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://github.com/Basseybd/react-task-tracker",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
