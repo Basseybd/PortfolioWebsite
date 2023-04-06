a869baccc0c6d599ec35ff8dabb308b644dacad1
Bassey Duke <bassey.bd@gmail.com>
2023-04-06 18:08:00 -0400

    fixed netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NetlifyForm, Honeypot } from "react-netlify-forms";
 
 export default function Contact() {
   const [name, setName] = React.useState("");
@@ -14,9 +13,9 @@ export default function Contact() {
       .join("&");
   }
 
-  function handleSubmit(e) {
-    <Honeypot />;
-    e.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
+ 
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -62,11 +61,15 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <NetlifyForm
+        <form
+          data-netlify="true"
+          action="/pages/success"
           name="contact"
+          method="post"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
+          <input type="hidden" name="form-name" value="contact" />
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
@@ -117,7 +120,7 @@ export default function Contact() {
           >
             Submit
           </button>
-        </NetlifyForm>
+        </form>
       </div>
     </section>
   );
