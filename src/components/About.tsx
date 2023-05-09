a525416e5504289d05d3deb5a6bfbedad9be3d98
Bassey Duke <bassey.bd@gmail.com>
2023-05-08 20:52:05 -0400

    updated headshot

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <img
-            src={require("../Assests/bassey.png")}
+            src={require("../Assests/bassey.jpg")}
             alt="bassey profile"
             className="h-52 w-52 rounded-full"
           />
