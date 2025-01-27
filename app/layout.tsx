58ddcd70bdce7e0e4c6623b41e183ee2f3d98278
Bassey Duke <bassey.bd@gmail.com>
2025-01-27 15:33:23 -0500

    fixed typo perview

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bassey's Porfolio",
+  title: "Bassey's Portfolio",
   description: "An insight to Bassey's software dev experience",
 };
 
